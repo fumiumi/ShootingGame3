82b14d20d54f944ebf40f22357b4569b02c77e07
fumiumi <raidenj6@gmail.com>
2025-01-14 14:59:25 +0900

    inputmanager完成。

---
+++ ShootingLib/InputManager.cpp
@@ -0,0 +1,120 @@
+#include "InputManager.h"
+#include "DxLib.h"
+
+// C++11以降のシングルトンパターンを使うのでこの処理は不要
+////タスクマネージャーのインスタンス初期化
+//InputManager *InputManager::instance_ = nullptr;
+
+/// <summary>
+/// InputManager.cppのみで使う定数
+/// </summary>
+namespace
+{
+/// <summary>
+/// ゲームボタンに対応したDXライブラリ側のキー定数
+/// リテラルはDXライブラリに定義されているものを使う
+/// </summary>
+const int kDxlibKeyKind[static_cast<int>(InputManager::GameKeyKind::kNum)] =
+{
+	//0x1C
+	KEY_INPUT_RETURN,   //Decide：Enterキー (D_DIK_RETURN)
+	//0x0E
+	KEY_INPUT_BACK,     //Cancel：BackSpaceキー (D_DIK_BACK)
+	//0xC8
+	KEY_INPUT_UP,       //Up：上キー (D_DIK_UP)
+	//0xD0
+	KEY_INPUT_DOWN,     //Down：下キー (D_DIK_DOWN)
+  //0xCB
+	KEY_INPUT_LEFT,     //Left：左キー (D_DIK_LEFT)
+  //0xCD
+	KEY_INPUT_RIGHT,    //Right：右キー (D_DIK_RIGHT)
+	//0x01
+	KEY_INPUT_ESCAPE,   //Pause：Escキー (D_DIK_ESCAPE)
+	//0x2A
+	KEY_INPUT_LSHIFT,   //PlayerMoveSlow：プレイヤー減速 左Shiftキー (D_DIK_LSHIFT)
+	//0x2C
+	KEY_INPUT_Z,        //PlayerFire：プレイヤー弾発射 Zキー (D_DIK_Z)
+};
+}
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+InputManager::InputManager()
+	: current_key_buffer_{ 0 },
+		pre_key_buffer_{ 0 }
+{
+}
+
+/// <summary>
+/// 現在のフレームのキー情報をすべて更新
+/// タスクマネージャーの更新の前に呼ぶ
+/// </summary>
+/// <param name="delta_time">前回実行フレームからの経過時間（秒）</param>
+void InputManager::UpdateKeyStateAll(float delta_time)
+{
+	// 現在のキー情報を更新する
+	// GetHitKeyStateAll 関数を使うにはまず
+	// char 型変数 256 個の配列を宣言します。 
+	// 必ず char 型で、数は 256 個でないといけません。
+	GetHitKeyStateAll(current_key_buffer_);
+}
+
+/// <summary>
+/// 現在のフレームのキー情報を記録
+/// フレームの最後に呼ぶ
+/// </summary>
+/// <param name="delta_time">前回実行フレームからの経過時間（秒）</param>
+void InputManager::RecordKeyStateAll(float delta_time)
+{
+	//現在のキー情報を記録しておく
+	for (int i = 0; i < kKeyBufferSize; ++i) {
+		pre_key_buffer_[i] = current_key_buffer_[i];
+	}
+}
+
+/// <summary>
+/// 引数のキーはこのフレームで押されたか
+/// </summary>
+/// <param name="key_kind">調べるキー</param>
+/// <returns>押されているならtrue</returns>
+bool InputManager::IsPushThisFlame(GameKeyKind game_key_kind)
+{
+	int target_key = kDxlibKeyKind[static_cast<int>(game_key_kind)];
+
+	//current_key_buffer_ と pre_key_buffer_ の値をチェックし
+	//このフレームでキーが押さた瞬間なら true を返す
+  if (current_key_buffer_[target_key] == 1 
+			&& pre_key_buffer_[target_key] == 0
+		 )
+  {
+    return true;
+  }
+	//このフレームでキーが押された瞬間ではないなら false を返す
+	else if(current_key_buffer_[target_key] == 0)
+	{
+    return false;
+	}
+}
+
+/// <summary>
+/// 引数のキーは押されているか
+/// </summary>
+/// <param name="game_key_kind">調べるキー</param>
+/// <returns>押されているならtrue</returns>
+bool InputManager::IsDown(GameKeyKind game_key_kind)
+{
+	int target_key = kDxlibKeyKind[static_cast<int>(game_key_kind)];
+
+	//current_key_buffer_の値をチェックし
+	//現在キーが押されているなら true を返す
+	//そうでないなら false を返す
+  if (current_key_buffer_[target_key] == 1)
+  {
+    return true;
+  }
+	else
+	{
+    return false;
+	}
+}
---
+++ ShootingLib/InputManager.h
@@ -0,0 +1,146 @@
+#pragma once
+
+/// <summary>
+/// インプットマネージャー処理
+/// </summary>
+/// <remarks>
+/// キーボードやパッドからの入力を管理する
+/// （サンプルなのでパッドは未実装）
+/// </remarks>
+class InputManager
+{
+public:
+	/// <summary>
+	/// 使用するゲームでのキーの種類
+	/// </summary>
+	enum class GameKeyKind
+	{
+		/// <summary>
+		/// 決定
+		/// </summary>
+		kDecide,
+		/// <summary>
+		/// キャンセル
+		/// </summary>
+		kCancel,
+		/// <summary>
+		/// 上ボタン
+		/// </summary>
+		kUp,
+		/// <summary>
+		/// 下ボタン
+		/// </summary>
+		kDown,
+		/// <summary>
+		/// 左ボタン
+		/// </summary>
+		kLeft,
+		/// <summary>
+		/// 右ボタン
+		/// </summary>
+		kRight,
+		/// <summary>
+		/// ポーズ
+		/// </summary>
+		kPause,
+		/// <summary>
+		/// プレイヤー減速
+		/// </summary>
+		kPlayerMoveSlow,
+		/// <summary>
+		/// プレイヤーバレット発射
+		/// </summary>
+		kPlayerFire,
+		/// <summary>
+		/// 総数
+		/// </summary>
+		kNum,
+	};
+
+	/// <summary>
+	/// InputManagerのインスタンスを取得する
+	/// </summary>
+	/// <returns></returns>
+	static InputManager &GetInstance() {
+		static InputManager instance;  // オブジェクトを直接生成
+		return instance;
+	}
+
+	/// <summary>
+	/// InputManagerのインスタンスを削除
+	/// ゲーム終了時に必ず呼ぶ
+	/// </summary>
+	static void Destory()
+	{
+		////生成済みなら削除
+		//if (instance_ != nullptr) {
+		//	delete instance_;
+		//	instance_ = nullptr;
+		//}
+    //自動破棄なので何もしない
+	}
+
+	/// <summary>
+	/// タスクの更新の前に呼ばれる関数
+	/// </summary>
+	/// <param name="delta_time">前回実行フレームからの経過時間（秒）</param>
+	void UpdateKeyStateAll(float delta_time);
+
+	/// <summary>
+	/// タスクの更新の後に呼ばれる関数
+	/// </summary>
+	/// <param name="delta_time">前回実行フレームからの経過時間（秒）</param>
+	void RecordKeyStateAll(float delta_time);
+
+	/// <summary>
+	/// 引数のキーはこのフレームで押されたか
+	/// </summary>
+	/// <param name="key_kind">調べるキー</param>
+	/// <returns>押されているならtrue</returns>
+	bool IsPushThisFlame(GameKeyKind game_key_kind);
+
+	/// <summary>
+	/// 引数のキーは押されているか
+	/// </summary>
+	/// <param name="game_key_kind">調べるキー</param>
+	/// <returns>押されているならtrue</returns>
+	bool IsDown(GameKeyKind game_key_kind);
+
+
+private:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	InputManager();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	virtual ~InputManager() = default;
+
+	/// <summary>
+	/// キー状態を格納するバッファのサイズ
+	/// </summary>
+	static const int kKeyBufferSize = 256;
+
+	///// <summary>
+	///// InputManagerのインスタンス
+	///// </summary>
+	//static InputManager *instance_;
+
+	/// <summary>
+	/// 現在のフレームのキー情報
+	/// GetHitKeyStateAll 関数を使うにはまず 
+	/// char 型変数 256 個の配列を宣言します。 
+	/// 必ず char 型で、数は 256 個でないといけません。
+	/// </summary>
+	char current_key_buffer_[kKeyBufferSize];
+
+	/// <summary>
+	/// 前回フレームのキー情報
+	/// GetHitKeyStateAll 関数を使うにはまず 
+	/// char 型変数 256 個の配列を宣言します。 
+	/// 必ず char 型で、数は 256 個でないといけません。
+	/// </summary>
+	char pre_key_buffer_[kKeyBufferSize];
+};
